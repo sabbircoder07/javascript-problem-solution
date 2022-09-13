08ac890cacc7640ec71f38c52b48d788412b8f68
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 19:11:38 +0600

    Create solution.js

---
+++ Pair Management/solution.js
@@ -0,0 +1,20 @@
+/*
+Pair Management
+
+Given two arguments, return an array which contains these two arguments.
+
+Examples
+makePair(1, 2) ➞ [1, 2]
+
+makePair(51, 21) ➞ [51, 21]
+
+makePair(512124, 215) ➞ [512124, 215]
+
+*/
+
+function makePair(num1, num2) {
+  let arr = [];
+  arr.push(num1);
+  arr.push(num2);
+  return arr;
+}
