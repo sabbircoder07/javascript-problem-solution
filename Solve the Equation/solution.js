10439a72b5e5ce58766a4a1fd77d4018db00ef28
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 20:14:24 +0600

    Create solution.js

---
+++ Solve the Equation/solution.js
@@ -0,0 +1,19 @@
+/*
+Solve the Equation
+
+Create a function that takes an equation (e.g. "1+1"), and returns the answer.
+
+Examples
+equation("1+1") ➞ 2
+
+equation("7*4-2") ➞ 26
+
+equation("1+1+1+1+1") ➞ 5
+Notes
+Supported operators are +, -, and *.
+
+*/
+
+function equation(s) {
+  return eval(s);
+}
