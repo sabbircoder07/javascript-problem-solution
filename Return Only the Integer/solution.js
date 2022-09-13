6fe5c1c020931880e07de8bcf5b2d4f2c25f1826
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 20:51:57 +0600

    Create solution.js

---
+++ Return Only the Integer/solution.js
@@ -0,0 +1,21 @@
+/*
+Return Only the Integer
+
+Write a function that takes an array of elements and returns only the integers.
+
+Examples
+returnOnlyInteger([9, 2, "space", "car", "lion", 16]) ➞ [9, 2, 16]
+
+returnOnlyInteger(["hello", 81, "basketball", 123, "fox"]) ➞ [81, 123]
+
+returnOnlyInteger([10, "121", 56, 20, "car", 3, "lion"]) ➞ [10, 56, 20, 3]
+
+returnOnlyInteger(["String",  true,  3.3,  1]) ➞ [1]
+Notes
+N/A
+
+*/
+
+function returnOnlyInteger(arr) {
+  return arr.filter((item) => Number.isInteger(item));
+}
