98f0f6d0b27d78e45e61a94cfe8d9f6342fc6306
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 22:25:22 +0600

    Create solution.js

---
+++ Are the Numbers Equal/solution.js
@@ -0,0 +1,22 @@
+/*
+Are the Numbers Equal?
+
+Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+Examples
+isSameNum(4, 8) ➞ false
+
+isSameNum(2, 2) ➞  true
+
+isSameNum(2, "2") ➞ false
+Notes
+Don't forget to return the result.
+*/
+
+function isSameNum(num1, num2) {
+  if (num1 === num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
