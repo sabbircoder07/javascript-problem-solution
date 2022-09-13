39234e582bf943ccbffac8353d01c691c91b517f
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 19:15:08 +0600

    Create solution.js

---
+++ Power Calculator/solution.js
@@ -0,0 +1,19 @@
+/*
+Power Calculator
+
+Create a function that takes voltage and current and returns the calculated power.
+
+Examples
+circuitPower(230, 10) ➞ 2300
+
+circuitPower(110, 3) ➞ 330
+
+circuitPower(480, 20) ➞ 9600
+Notes
+Requires basic calculation of electrical circuits (see Resources for info).
+
+*/
+
+function circuitPower(voltage, current) {
+  return voltage * current;
+}
