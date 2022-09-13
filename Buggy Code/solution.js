528fc5f0ff1e3182a71b4f0e4f8c3645ea79f35a
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-13 19:37:50 +0600

    Create solution.js

---
+++ Buggy Code/solution.js
@@ -0,0 +1,21 @@
+/*
+Buggy Code
+
+The challenge is to try and fix this buggy code, given the inputs true and false. See the examples below for the expected output.
+
+Examples
+has_bugs(true) ➞ "sad days"
+
+has_bugs(false) ➞ "it's a good day"
+Notes
+Don't overthink this challenge (look at the syntax and correct it)
+
+*/
+
+function has_bugs(buggy_code) {
+  if (buggy_code) {
+    return "sad days";
+  } else {
+    return "it's a good day";
+  }
+}
