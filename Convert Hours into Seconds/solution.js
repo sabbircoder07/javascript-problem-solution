ceb320e1fd877a862b026e3825ddc503bfcacd68
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-14 11:42:58 +0600

    Create solution.js

---
+++ Convert Hours into Seconds/solution.js
@@ -0,0 +1,22 @@
+/*
+Convert Hours into Seconds
+
+Write a function that converts hours into seconds.
+
+Examples
+howManySeconds(2) ➞ 7200
+
+howManySeconds(10) ➞ 36000
+
+howManySeconds(24) ➞ 86400
+Notes
+60 seconds in a minute, 60 minutes in an hour
+Don't forget to return your answer.
+
+*/
+
+function howManySeconds(hours) {
+  return hours * 3600;
+}
+
+console.log(howManySeconds(24));
