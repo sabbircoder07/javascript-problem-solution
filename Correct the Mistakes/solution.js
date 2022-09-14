f0190042524f027f480520ac09160b72169b0c01
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-14 10:30:39 +0600

    Create solution.js

---
+++ Correct the Mistakes/solution.js
@@ -0,0 +1,27 @@
+/*
+Correct the Mistakes
+
+Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+Examples
+squared(5) ➞ 25
+
+squared(9) ➞ 81
+
+squared(100) ➞ 10000
+Notes
+READ EVERY WORD CAREFULLY, CHARACTER BY CHARACTER!
+Don't overthink this challenge; it's not supposed to be hard.
+
+
+function squaed(b) {
+	return a * a
+}
+
+*/
+
+function squared(b) {
+  return b * b;
+}
+
+console.log(squared(100));
