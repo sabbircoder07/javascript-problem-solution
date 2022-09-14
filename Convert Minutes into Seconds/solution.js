a091e8d10109bf0ab32524518e4957f06b27b66b
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-14 11:30:39 +0600

    Create solution.js

---
+++ Convert Minutes into Seconds/solution.js
@@ -0,0 +1,23 @@
+/*
+Convert Minutes into Seconds
+
+Write a function that takes an integer minutes and converts it to seconds.
+
+Examples
+convert(5) ➞ 300
+
+convert(3) ➞ 180
+
+convert(2) ➞ 120
+Notes
+Don't forget to return the result.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+*/
+
+function convert(minutes) {
+  return minutes * 60;
+}
+
+console.log(convert(2));
