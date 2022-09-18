da1b9d629a523bb124becab097d2568eedebb5ee
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-18 12:25:04 +0600

    Create solution.js

---
+++ Recursion: Length of a String/solution.js
@@ -0,0 +1,23 @@
+/*
+Recursion: Length of a String
+
+Write a function that returns the length of a string. Make your function recursive.
+
+Examples
+length("apple") ➞ 5
+
+length("make") ➞ 4
+
+length("a") ➞ 1
+
+length("") ➞ 0
+Notes
+Check the Resources tab for info on recursion.
+*/
+
+function length(str) {
+  if (str === "") return 0;
+  return length(str.substr(1)) + 1;
+}
+
+console.log(length(""));
