0ac0a4b80be501799fec3359c51ed6000124cb54
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-19 11:30:46 +0600

    Create solution.js

---
+++ Say Hello to Guests/solution.js
@@ -0,0 +1,33 @@
+/*
+Say Hello to Guests
+
+In this exercise you will have to:
+
+Take a list of names.
+Add "Hello" to every name.
+Make one big string with all greetings.
+The solution should be one string with a comma in between every "Hello (Name)".
+
+Examples
+greetPeople(["Joe"]) ➞ "Hello Joe"
+
+greetPeople(["Angela", "Joe"]) ➞ "Hello Angela, Hello Joe"
+
+greetPeople(["Frank", "Angela", "Joe"]) ➞ "Hello Frank, Hello Angela, Hello Joe"
+Notes
+Each greeting has to be separated with a comma and a space.
+If you're given an empty array [], return an empty string "".
+*/
+
+function greetPeople(names) {
+  let myGreetPeople = "";
+  if (names.length === 0) {
+    myGreetPeople = `""`;
+  } else {
+    myGreetPeople = names.map((x) => "Hello" + " " + x).join(", ");
+  }
+
+  return myGreetPeople;
+}
+
+console.log(greetPeople([]));
