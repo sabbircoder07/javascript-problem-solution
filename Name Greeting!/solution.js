9857a4b0848e808692e3624056f7463d148b1cd8
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2022-09-19 13:18:11 +0600

    Create solution.js

---
+++ Name Greeting!/solution.js
@@ -0,0 +1,22 @@
+/*
+Name Greeting!
+
+Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function, use an arrow function.
+
+Examples
+helloName("Gerald") ➞ "Hello Gerald!"
+
+helloName("Tiffany") ➞ "Hello Tiffany!"
+
+helloName("Ed") ➞ "Hello Ed!"
+Notes
+The input is always a name (as string).
+Don't forget the exclamation mark!
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+*/
+
+const helloName = (name) => `Hello ${name}!`;
+
+console.log(helloName("Ed"));
