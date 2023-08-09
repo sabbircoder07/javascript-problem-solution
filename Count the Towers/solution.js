f777b96d68b506f15a253489e8ed978239d0fa82
Sabbir Ahmed <64855840+sabbircoder07@users.noreply.github.com>
2023-08-09 13:02:24 +0600

    Update solution.js

--- Count the Towers/solution.js
+++ Count the Towers/solution.js
@@ -1,8 +1,6 @@
 /*
 Count the Towers
-
 Create a function that counts the number of towers.
-
 Examples
 count_towers([
   ["     ##         "],
@@ -27,11 +25,20 @@ You are given a 2D matrix.
 Towers are two characters in length.
 Towers are made only of the character #.
 Some tests have no towers, return 0.
-
 */
 
 function countTowers(towers) {
-  return (
-    towers[towers.length - 1][0].split("").filter((x) => x === "#").length / 2
-  );
+  return towers[towers.length - 1][0]
+    .trim()
+    .split(' ')
+    .filter(item => item !== '').length;
 }
+
+console.log(
+  countTowers([
+    ['                         ##'],
+    ['##             ##   ##   ##'],
+    ['##        ##   ##   ##   ##'],
+    ['##   ##   ##   ##   ##   ##'],
+  ])
+);
